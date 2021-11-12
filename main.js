a348e51bcc5c68d52dae4af1282056a1ef95eab7
Deem Ali <92226852+Deemowe@users.noreply.github.com>
2021-11-12 19:08:31 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+//  JS fuctionalluty 
+
+
+ 
+ 
+    function myFunction() {
+      var x = document.getElementById("myDIV");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+   
+    
