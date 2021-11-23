7b4d69c5e72a4ff44e562c4bb19f563ab9102861
wajdsaleh <74684120+wajdsaleh@users.noreply.github.com>
2021-11-23 14:25:08 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+function myFunction1() {
+  var dots = document.getElementById("dots1");
+  var moreText = document.getElementById("more1");
+  var btnText = document.getElementById("myBtn1");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "اقـرأ أكثـر";
+    moreText.style.display = "none";
+  } 
+  else {
+    dots.style.display = "none";
+    btnText.style.display="none";
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunction2() {
+  var dots = document.getElementById("dots2");
+  var moreText = document.getElementById("more2");
+  var btnText = document.getElementById("myBtn2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "اقـرأ أكثـر";
+    moreText.style.display = "none";
+  } 
+  else {
+    dots.style.display = "none";
+    btnText.style.display="none";
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunction3() {
+  var dots = document.getElementById("dots3");
+  var moreText = document.getElementById("more3");
+  var btnText = document.getElementById("myBtn3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "اقـرأ أكثـر";
+    moreText.style.display = "none";
+  } 
+  else {
+    dots.style.display = "none";
+    btnText.style.display="none";
+    moreText.style.display = "inline";
+  }
+}
