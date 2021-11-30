bc478f6caa5905c1f55e87a3b6fd276223b76e81
Wesam <74800962+wesamhamad@users.noreply.github.com>
2021-11-30 14:12:50 +0300

    JavaScript files

---
+++ js/store.js
@@ -0,0 +1,364 @@
+function Ahmad1()
+{
+    localStorage.setItem("img1","images/STORE/Ahmad1.jpg");  
+    localStorage.setItem("name","الفنان: أحمد المغلوث");  
+    
+} 
+function Ahmad2()
+{
+    localStorage.setItem("img1","images/STORE/Ahmad2.jpg"); 
+    localStorage.setItem("name","الفنان: أحمد المغلوث");
+}
+function Ahmad3()
+{
+    localStorage.setItem("img1","images/STORE/Ahmad3.jpg"); 
+    localStorage.setItem("name","الفنان: أحمد المغلوث");  
+}
+function AhmadF1()
+{
+    localStorage.setItem("img1","images/STORE/AhmadF1.jpg"); 
+    localStorage.setItem("name","الفنان:  أحمد فلمبان");  
+}
+function AhmadF2()
+{
+    localStorage.setItem("img1","images/STORE/AhmadF2.jpg");  
+    localStorage.setItem("name","الفنان:  أحمد فلمبان");  
+
+}
+function AhmadF3()
+{
+    localStorage.setItem("img1","images/STORE/AhmadF3.jpg"); 
+    localStorage.setItem("name","الفنان:  أحمد فلمبان");  
+ 
+}
+function Ali1()
+{
+    localStorage.setItem("img1","images/STORE/Ali1.jpg");  
+    localStorage.setItem("name","الفنان:  علي الرزيزاء ");  
+
+}
+function Ali2()
+{
+    localStorage.setItem("img1","images/STORE/Ali2.jpeg");  
+    localStorage.setItem("name","الفنان:  علي الرزيزاء ");  
+
+}
+function Ali3()
+{
+    localStorage.setItem("img1","images/STORE/Ali3.jpg");  
+    localStorage.setItem("name","الفنان:  علي الرزيزاء ");  
+
+}
+function Alshaik1()
+{
+    localStorage.setItem("img1","images/STORE/Alshaik1.jpg"); 
+    localStorage.setItem("name","الفنان:  عبدالله الشيخ ");  
+ 
+}
+function Alshaik2()
+{
+    localStorage.setItem("img1","images/STORE/Alshaik2.jpg"); 
+    localStorage.setItem("name","الفنان:  عبدالله الشيخ ");  
+ 
+}
+function Alshaik3()
+{
+    localStorage.setItem("img1","images/STORE/Alshaik3.jpg");  
+    localStorage.setItem("name","الفنان:  عبدالله الشيخ ");  
+
+}
+function Alsulaiman1()
+{
+    localStorage.setItem("img1","images/STORE/Alsulaiman1.jpg");  
+    localStorage.setItem("name","الفنان:  عبدالرحمن السليمان ");  
+
+}
+function Alsulaiman2()
+{
+    localStorage.setItem("img1","images/STORE/Alsulaiman2.jpg");  
+    localStorage.setItem("name","الفنان:  عبدالرحمن السليمان ");  
+
+}
+function Alsulaiman3()
+{
+    localStorage.setItem("img1","images/STORE/Alsulaiman3.jpg");  
+    localStorage.setItem("name","الفنان:  عبدالرحمن السليمان ");  
+
+}
+function Alyaa1()
+{
+    localStorage.setItem("img1","images/STORE/Alyaa1.jpeg");  
+    localStorage.setItem("name","الفنانة: علياء السلطان ");  
+
+}
+function Alyaa2()
+{
+    localStorage.setItem("img1","images/STORE/Alyaa2.jpeg"); 
+    localStorage.setItem("name","الفنانة: علياء السلطان ");  
+ 
+}
+function Alyaa3()
+{
+    localStorage.setItem("img1","images/STORE/Alyaa3.jpeg"); 
+    localStorage.setItem("name","الفنانة: علياء السلطان ");  
+ 
+}
+function Diyaa1()
+{
+    localStorage.setItem("img1","images/STORE/Diyaa1.jpg");  
+    localStorage.setItem("name","الفنان: ضياء عزيز ");  
+
+}
+function Diyaa2()
+{
+    localStorage.setItem("img1","images/STORE/Diyaa2.jpg"); 
+    localStorage.setItem("name","الفنان: ضياء عزيز ");   
+}
+function Diyaa3()
+{
+    localStorage.setItem("img1","images/STORE/Diyaa3.jpg");  
+    localStorage.setItem("name","الفنان: ضياء عزيز ");  
+}
+function Fuaad1()
+{
+    localStorage.setItem("img1","images/STORE/Fuaad1.jpg");  
+    localStorage.setItem("name","الفنان: فؤاد مغربل ");  
+
+}
+function Fuaad2()
+{
+    localStorage.setItem("img1","images/STORE/Fuaad2.jpg");
+    localStorage.setItem("name","الفنان: فؤاد مغربل ");  
+  
+}
+function Fuaad3()
+{
+    localStorage.setItem("img1","images/STORE/Fuaad3.jpg");  
+    localStorage.setItem("name","الفنان: فؤاد مغربل ");  
+
+}
+function Garem1()
+{
+    localStorage.setItem("img1","images/STORE/Garem1.jpg");  
+    localStorage.setItem("name","الفنان: عبدالناصر غارم  ");  
+
+}
+function Garem2()
+{
+    localStorage.setItem("img1","images/STORE/Garem2.jpg"); 
+    localStorage.setItem("name","الفنان: عبدالناصر غارم  ");  
+ 
+}
+function Garem3()
+{
+    localStorage.setItem("img1","images/STORE/Garem3.jpg");  
+    localStorage.setItem("name","الفنان: عبدالناصر غارم  ");      
+}
+function Layan1()
+{
+    localStorage.setItem("img1","images/STORE/Layan1.jpeg");  
+    localStorage.setItem("name","الفنانة: ليان الجريش ");  
+}
+function Layan2()
+{
+    localStorage.setItem("img1","images/STORE/Layan2.jpeg");  
+    localStorage.setItem("name","الفنانة: ليان الجريش ");  
+}
+function Layan3()
+{
+    localStorage.setItem("img1","images/STORE/Layan3.jpeg");  
+    localStorage.setItem("name","الفنانة: ليان الجريش ");  
+}
+function Mothla1()
+{
+    localStorage.setItem("img1","images/STORE/Mothla1.jpeg");  
+    localStorage.setItem("name","الفنانة:  مُثلى النوشان ");  
+}
+function Mothla2()
+{
+    localStorage.setItem("img1","images/STORE/Mothla2.jpeg");  
+    localStorage.setItem("name","الفنانة:  مُثلى النوشان "); 
+}
+function Mothla3()
+{
+    localStorage.setItem("img1","images/STORE/Mothla3.jpeg");
+    localStorage.setItem("name","الفنانة:  مُثلى النوشان ");   
+}
+function Othman1()
+{
+    localStorage.setItem("img1","images/STORE/Othman1.jpg"); 
+    localStorage.setItem("name","الفنان: عثمان الخزيم ");      
+ 
+}
+function Othman2()
+{
+    localStorage.setItem("img1","images/STORE/Othman2.jpg"); 
+    localStorage.setItem("name","الفنان: عثمان الخزيم ");      
+ 
+}
+function Othman3()
+{
+    localStorage.setItem("img1","images/STORE/Othman3.jpg"); 
+    localStorage.setItem("name","الفنان: عثمان الخزيم ");       
+}
+function Radwi1()
+{
+    localStorage.setItem("img1","images/STORE/Radwi1.jpg"); 
+    localStorage.setItem("name","الفنان: عبدالحليم رضوي ");       
+ 
+}
+function Radwi2()
+{
+    localStorage.setItem("img1","images/STORE/Radwi2.jpg");  
+    localStorage.setItem("name","الفنان: عبدالحليم رضوي ");       
+
+}
+function Radwi3()
+{
+    localStorage.setItem("img1","images/STORE/Radwi3.jpg"); 
+    localStorage.setItem("name","الفنان: عبدالحليم رضوي ");       
+ 
+}
+function Saad1()
+{
+    localStorage.setItem("img1","images/STORE/Saad1.jpg");  
+    localStorage.setItem("name","الفنان:  سعد العبيد ");       
+
+}
+function Saad2()
+{
+    localStorage.setItem("img1","images/STORE/Saad2.jpg");  
+    localStorage.setItem("name","الفنان:  سعد العبيد ");       
+
+}
+function Saad3()
+{
+    localStorage.setItem("img1","images/STORE/Saad3.jpg"); 
+    localStorage.setItem("name","الفنان:  سعد العبيد ");       
+ 
+}
+function Saleh1()
+{
+    localStorage.setItem("img1","images/STORE/Saleh1.jpg");  
+    localStorage.setItem("name","الفنان:  صالح النقيدان  ");       
+
+}
+function Saleh2()
+{
+    localStorage.setItem("img1","images/STORE/Saleh2.jpg");  
+    localStorage.setItem("name","الفنان:  صالح النقيدان  ");       
+}
+function Saleh3()
+{
+    localStorage.setItem("img1","images/STORE/Saleh3.jpg");  
+    localStorage.setItem("name","الفنان:  صالح النقيدان  ");       
+
+}
+function Sameer1()
+{
+    localStorage.setItem("img1","images/STORE/Sameer1.jpg");  
+    localStorage.setItem("name","الفنان:  سمير الدهام  ");       
+
+}
+function Sameer2()
+{
+    localStorage.setItem("img1","images/STORE/Sameer2.jpg");  
+    localStorage.setItem("name","الفنان:  سمير الدهام  ");       
+
+}
+function Sameer3()
+{
+    localStorage.setItem("img1","images/STORE/Sameer3.jpg");  
+    localStorage.setItem("name","الفنان:  سمير الدهام  ");       
+
+}
+function Sarah_Abdu()
+{
+    localStorage.setItem("img1","images/STORE/Sarah_Abdu.jpg"); 
+    localStorage.setItem("name","الفنانة:  سارة أبو عبدالله  "); 
+ 
+}
+function Sarah_Abdu3()
+{
+    localStorage.setItem("img1","images/STORE/Sarah_Abdu3.jpg");  
+    localStorage.setItem("name","الفنانة:  سارة أبو عبدالله  "); 
+
+}
+function Sarah_Abdu4()
+{
+    localStorage.setItem("img1","images/STORE/Sarah_Abdu4.jpg");  
+    localStorage.setItem("name","الفنانة:  سارة أبو عبدالله  "); 
+
+}
+function shadia_alem()
+{
+    localStorage.setItem("img1","images/STORE/shadia_alem.jpg");  
+    localStorage.setItem("name","الفنانة:  شادية عالِم  "); 
+
+}
+function shadia_alem2()
+{
+    localStorage.setItem("img1","images/STORE/shadia_alem2.jpg");  
+    localStorage.setItem("name","الفنانة:  شادية عالِم  "); 
+
+}
+function shadia_alem3()
+{
+    localStorage.setItem("img1","images/STORE/shadia_alem3.jpg");  
+    localStorage.setItem("name","الفنانة:  شادية عالِم  "); 
+
+}
+function Shouq1()
+{
+    localStorage.setItem("img1","images/STORE/Shouq1.jpg");  
+    localStorage.setItem("name","الفنانة:  شوق الجابر  "); 
+
+}
+function Shouq2()
+{
+    localStorage.setItem("img1","images/STORE/Shouq2.jpg"); 
+    localStorage.setItem("name","الفنانة:  شوق الجابر  "); 
+ 
+}
+function Shouq3()
+{
+    localStorage.setItem("img1","images/STORE/Shouq3.jpg");  
+    localStorage.setItem("name","الفنانة:  شوق الجابر  "); 
+
+}
+function Tahh1()
+{
+    localStorage.setItem("img1","images/STORE/Tahh1.jpeg");  
+    localStorage.setItem("name","الفنان:  طه الصبان  "); 
+
+}
+function Tahh2()
+{
+    localStorage.setItem("img1","images/STORE/Tahh2.jpg");  
+    localStorage.setItem("name","الفنان:  طه الصبان  "); 
+
+}
+function Tahh3()
+{
+    localStorage.setItem("img1","images/STORE/Tahh3.jpg"); 
+    localStorage.setItem("name","الفنان:  طه الصبان  "); 
+ 
+}
+function Zuhair1()
+{
+    localStorage.setItem("img1","images/STORE/Zuhair1.jpg");  
+    localStorage.setItem("name","الفنان:  زهير طوله  "); 
+
+}
+function Zuhair2()
+{
+    localStorage.setItem("img1","images/STORE/Zuhair2.jpg");  
+    localStorage.setItem("name","الفنان:  زهير طوله  "); 
+
+}
+function Zuhair3()
+{
+    localStorage.setItem("img1","images/STORE/Zuhair3.jpg"); 
+    localStorage.setItem("name","الفنان:  زهير طوله  "); 
+ 
+}
